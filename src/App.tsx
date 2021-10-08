040bfb887014246c6ad3c76a1cb4d9e2cfc83f8d
Alexis Brissard <alexis.brissard@developpement-durable.gouv.fr>
2021-10-08 11:27:49 +0200

    Scroll only right side

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,27 @@ const params = new URLSearchParams(window.location.search)
 const apiurl = params.get("api-url")  || 'https://alexisb.pythonanywhere.com/getAides/'
 console.log("Using api url: ", apiurl)
 
+type AidePublique =  {
+  "titre_aide": string,
+  "aide_detail": string,
+  "aide_detail_clean": string,
+  "contact": string
+}
+
 type reponseType = {
   "fichier_aides": string,
   "descriptionSU": string,
   "fichier_vocab": string,
   "nb_aides": number,
-  "resultats_aides": [
-    {
-      "titre_aide": string,
-      "aide_detail": string
-    }
-  ]
+  "resultats_aides": AidePublique[]
 }
 
-function OneResult(props: {txt:string}) {
-    return <div style={{border: "1px solid black", padding: "10px 20px"}}>{props.txt}</div>
+function OneResult(props: {aide: AidePublique}) {
+    return <div style={{border: "1px solid black", padding: "10px 20px"}}>
+      <div>{props.aide.titre_aide}</div>
+      
+      <div dangerouslySetInnerHTML={{__html: props.aide.contact}}>{}</div>
+    </div>
 }
 
 function buildAidesRequest(description : string) {
@@ -56,7 +62,7 @@ function App() {
   function updateReponse() {
     console.log("updating results")
     setReponse(null);
-    buildAidesRequest(descriptionStartup).then(json => {
+    buildAidesRequest(descriptionStartup || "Nous sommes une startup spécialisé dans le tri des déchets métalliques").then(json => {
       setReponse(json)
       window.lastApiResponse = json
     })
@@ -74,13 +80,13 @@ function App() {
       <div className="description-startup">
         <textarea
           onChange={e => setDescriptionStartup(e.target.value)}
-          style={{width: "100%", height: "100%"}}
+          style={{width: "50vw", height: "50vh"}}
           value={descriptionStartup}
           placeholder="ex: Nous sommes une startup spécialisé dans le tri des déchets métalliques et...">
         </textarea>
       </div>
-      <div style={{border: "1px solid black"}} className="resultats">
-        {reponse ? reponse.resultats_aides.map(x => <OneResult txt={x.titre_aide}/>) : "It's loading..."}
+      <div className="resultats">
+        {reponse ? reponse.resultats_aides.map(x => <OneResult aide={x}/>) : "It's loading..."}
       </div>
     </div>
   );
