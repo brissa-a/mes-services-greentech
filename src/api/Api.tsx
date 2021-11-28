8401ae34446cec324e07822f107556c41cfc6c6c
Alexis Brissard <alexis.brissard@developpement-durable.gouv.fr>
2021-11-28 19:37:39 +0100

    Type defined from API ex

--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -1,62 +1,22 @@
 import mockApiResponse from './mock_api_resp.json'
 import {useMockResponse, apiurl, max_results} from "../UrlSearchParam"
 
-export type Aide = {
-    "titre_aide": string,
-    "aide_detail": string,
-    "aide_detail_clean": string,
-    "score": number,
-    "contact": string,
-    "funding_source_url": string
-}
+/*
+  From old FTE file
+*/
+export type Aide = typeof mockApiResponse.cards.aides[0]
 
-export type CollectiviteScore = null | {
-    "proportion_numerateur": string,
-    "proportion_denominateur": string,
-    "proportion": number
-}
+/*
+  Deduced from DECP
+*/
+export type Collectivite = typeof mockApiResponse.cards.collectivites[0]
 
-export type Collectivite = {
-    "nom": string,
-    "score_details": {
-        "innovation": CollectiviteScore,
-        "sous_les_seuils": CollectiviteScore,
-        "petites_entreprises": CollectiviteScore
-    },
-    "score": number
-}
+/*
+  deduced from DECP
+*/
+export type Marche = typeof mockApiResponse.cards.marches[0]
 
-export type Marche = {
-    "libelle": string,                  //"MS_S\u00e9rigraphies, vitrages",
-    "domaine": string,                  //"Transport de personnes et de biens - V\u00e9hicules",
-    "groupe_marchandise_code": string,  //"34.03.01",
-    "groupe_marchandise_nom": string,   //"PIECES DETACHEES ET ACCESSOIRES VEHICULES TERRESTRES",
-    "acheteur": string,                 //"Minist\u00e8re de l'Int\u00e9rieur",
-    "entite_porteuse": string,          //"Etablissement central logistique de la police nationale",
-    "contexte": string,                 //"Renouvellement march\u00e9 existant",
-    "duree_mois": number,               //48.0,
-    "annee": number,                    //2023.0,
-    "depense_annualisee": string,       //"Compris entre 200 000 \u20ac et 1 M\u20ac",
-    "depense_min": number | null,              //200000,
-    "depense_max": number,              //1000000,
-    "score": number,                    //1.6401309967041016
-}
-
-export type ApiResponse = {
-    "fichier_aides": string,
-    "fichier_achats_previs": string,
-    "fichier_embeddings_achats": string,
-    "fichier_decp": string,
-    "descriptionSU": string,
-    "fichier_vocab": string,
-    "nb_aides": number,
-    "nb_achats_previs": number,
-    "cards": {
-        "collectivites": Collectivite[],
-        "aides": Aide[],
-        "marches": Marche[]
-    }
-}
+export type ApiResponse = typeof mockApiResponse
 
 export function buildSearchAnythingRequest(description: string) {
     if (useMockResponse) {
