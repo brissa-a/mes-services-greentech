5fa46f8eb800826936eb24c26ec0d0a71a15987c
Alexis Brissard <alexis.brissard@developpement-durable.gouv.fr>
2021-11-29 12:23:00 +0100

    Fix api call getAides => getCards + query json template

--- src/UrlSearchParam.tsx
+++ src/UrlSearchParam.tsx
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(window.location.search)
-export const apiurl = params.get("api-url") || 'https://alexisb.pythonanywhere.com/getAides/'
+export const apiurl = params.get("api-url") || 'https://alexisb.pythonanywhere.com/getCards/'
 export const max_results_str = params.get("max-results")
 export const max_results = max_results_str && JSON.parse(max_results_str) || 30
 export const defaultDescription = params.get("description") || "Nous sommes une startup spécialisé dans le tri des déchets métalliques"
--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -1,5 +1,5 @@
 import mockApiResponse from './mock_api_resp.json'
-import {useMockResponse, apiurl, max_results} from "../UrlSearchParam"
+import { useMockResponse, apiurl, max_results } from "../UrlSearchParam"
 
 /*
   From old FTE file
@@ -19,26 +19,34 @@ export type Marche = typeof mockApiResponse.cards.marches[0]
 export type ApiResponse = typeof mockApiResponse
 
 export function buildSearchAnythingRequest(description: string) {
-    if (useMockResponse) {
-      return  new Promise<ApiResponse>(res => setTimeout(() => res(mockApiResponse), 3000))
-    } else {
-      return fetch(apiurl, {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          "fichier_aides": "Aides_detailsandname.xlsx",
-          "descriptionSU": description,
-          "fichier_vocab": "vocab.pkl",
-          "nb_aides": max_results,
-          "resultats_aides": [],
-          "score_max": 0
-        })
+  if (useMockResponse) {
+    return new Promise<ApiResponse>(res => setTimeout(() => res(mockApiResponse), 3000))
+  } else {
+    return fetch(apiurl, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "fichier_aides": "Aides_detailsandname.xlsx",
+        "fichier_achats_previs": "Programmation_2021-2024.xlsx",
+        "fichier_decp": "decp_score.csv",
+        "descriptionSU": "Je suis une entreprise de traitement des déchets",
+        "fichier_vocab": "vocab.pkl",
+        "fichier_embeddings_achats": "embeddings_achats_previs.pkl",
+        "nb_aides": 10,
+        "nb_achats_previs": 12,
+        "nb_acheteur": 10,
+        "cards": {
+          "collectivites": [],
+          "aides": [],
+          "marches": []
+
+        }
       })
-      .then(resp => resp.json())      
-    }
+    })
+      .then(resp => resp.json())
   }
-  
-  
+}
+
